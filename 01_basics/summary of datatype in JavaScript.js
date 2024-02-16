260946cb3befd8f06f298e7cf8fb8f1a61af809e
ReStart <157919901+codedropped@users.noreply.github.com>
2024-02-16 13:22:39 +0000

    summary of datatypes in JS

---
+++ 01_basics/summary of datatype in JavaScript.js
@@ -0,0 +1,10 @@
+/*****************Data Type Summary******************/
+// Data types in Java Script 
+
+// Two types of Data Types
+
+// Primitive Data Types
+// These are the following data types::
+// String, Number, Boolean, 
+// Non Primitive Data Types
+// Arrays, Objects, Functions
