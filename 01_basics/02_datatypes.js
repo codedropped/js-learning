86f6cdf40871d67a40334d9dee1e802debe399c6
codedropped <157919901+codedropped@users.noreply.github.com>
2024-02-16 08:10:20 +0000

    container config done

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -2,5 +2,34 @@
 // alert("hello"); // we are using nodejs, not browser
 // alert(3+3); // we are using nodejs, not browser
 
-console.log(3+3)
+console.log(3
+    +
+    3) // code readability should be high
 console.log("Akon")
+
+
+let name = "Akon" // String data type
+let age = 15 // Number data type
+let isLoggedIn = true // Boolean data type
+let state = null // null data type
+let gender; // undefined data type
+
+
+//number => 2 to the power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+//object
+
+console.log(typeof "Akon"); // output => string
+console.log(typeof age); // output => number
+console.log(typeof null); // output => object
+console.log(typeof undefined); // output => undefined
+
+
+
