88a89e687548994b3c881b0705a4fd071c967c5b
codedropped <157919901+codedropped@users.noreply.github.com>
2024-02-16 11:35:04 +0000

    container config done

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -1,46 +1,47 @@
 // Comparison ans will come within boolean datatypes true or false
-console.log(2 < 1 ); // output => 
 console.log("")
-console.log(2 > 1 );
+console.log(2 < 1 ); // output => false
 console.log("")
-console.log(2 <= 1 );
+console.log(2 > 1 ); // output => true
 console.log("")
-console.log(2 >= 1 );
+console.log(2 <= 1 ); // output => false
 console.log("")
-console.log(2 == 1 );
+console.log(2 >= 1 ); // output => true
 console.log("")
-console.log(2 === 1 );
+console.log(2 == 1 ); // output => false
 console.log("")
-console.log(2 != 1 );
+console.log(2 === 1 ); // output => false
 console.log("")
-
-console.log("2" < 1 );
-console.log("")
-console.log(1 > "3");
+console.log(2 != 1 ); // output => true
 console.log("")
 
-console.log(null > 0);
+console.log("2" < 1 ); // output => false // JS automatically convert string to number
 console.log("")
-console.log(null > 0);
-console.log("")
-console.log(null == 0);
-console.log("")
-console.log(null != 0);
-console.log("")
-console.log(null <= 0);
-console.log("")
-console.log(null >= 0);
+console.log(1 > "3"); // output => false
 console.log("")
 
-console.log(undefined > 0);
+console.log(null > 0); // output => false
 console.log("")
-console.log(undefined > 0);
+console.log(null < 0); // output => false
 console.log("")
-console.log(undefined == 0);
+console.log(null == 0); // output => false
 console.log("")
-console.log(undefined != 0);
+console.log(null != 0); // output => true
 console.log("")
-console.log(undefined <= 0);
+console.log(null <= 0); // output => true
 console.log("")
-console.log(undefined >= 0);
+console.log(null >= 0); // output => true
+console.log("")
+
+console.log(undefined > 0); // output => false
+console.log("")
+console.log(undefined > 0); // output => false
+console.log("")
+console.log(undefined == 0); // output => false
+console.log("")
+console.log(undefined != 0); // output => true
+console.log("")
+console.log(undefined <= 0); // output => false
+console.log("")
+console.log(undefined >= 0); // output => false
 console.log("")
