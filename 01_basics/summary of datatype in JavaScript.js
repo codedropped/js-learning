31e00cc31479713abb0e48634e296bf2243fa5e0
ReStart <157919901+codedropped@users.noreply.github.com>
2024-02-16 13:31:37 +0000

    summary of data type in JavaScript

--- 01_basics/summary of datatype in JavaScript.js
+++ 01_basics/summary of datatype in JavaScript.js
@@ -5,6 +5,30 @@
 
 // Primitive Data Types
 // These are the following data types::
-// String, Number, Boolean, 
+// String, Number, Boolean, undefined, null, bigInt, symbol
+
+let string = "Akon"
+let number = 23
+let boolean = true
+let undefined
+let nullValue = null
+let bigInt = 354648946464313646n
+let symbol = Symbol("321")
+let anotherSymbol = Symbol("321")
+
+
+
+
 // Non Primitive Data Types
 // Arrays, Objects, Functions
+
+let arrays = ["Akon", 5, "Male"]
+let object = {
+    name: "Akon",
+    age: 5,
+    gender: "Male"
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+}
