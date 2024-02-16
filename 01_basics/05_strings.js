7f33094e7efdbb3eea639f4276cbc9136113da7d
ReStart <157919901+codedropped@users.noreply.github.com>
2024-02-16 17:54:27 +0000

    strings

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -1,1 +1,29 @@
+// making console.log() s shortcut normal function with variable
+let print = function(abc){
+    console.log(abc)
+}
+console.log(typeof print); // display the type of this variable
+
+// making console.log() s shortcut arrow function with variable
+let display = (xyz) => {
+    console.log(xyz + " (by display function)")
+}
+console.log(typeof display);// display the type of this variable
+display("") // we can use print("") function as well
+
 const name = "Akon"
+const nameObj = new String(name)
+display(`Length of name is ${name.length}`)
+print(nameObj.toUpperCase());
+display(nameObj.toLowerCase());
+print(`Length of nameObj is ${nameObj.length}`);
+
+const newName = new String('Barak Obama')
+
+display(name);
+print(nameObj);
+display(newName);
+
+print(nameObj)
+display(newName)
+
