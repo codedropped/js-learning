b6a251443353c47a74b85d3886d8ceef9c7bff66
codedropped <157919901+codedropped@users.noreply.github.com>
2024-02-16 09:53:52 +0000

    container config done

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,53 @@
+let score = 33;
+
+console.log("") // we are using this to make an space into the console to understand our output.
+
+console.log(score); // output => 33
+console.log(typeof score); // typeof's systanx // output => number
+console.log(typeof(score)); // typeof as method // output => number
+console.log("") // we are using this to make an space into the console to understand our output.
+// output => blank space
+
+let numberToString = String(score); //converting score value as String // output => 33 (as string)
+console.log(numberToString) // output => 33 (as string)
+console.log(typeof numberToString); // output => string
+console.log("") // we are using this to make an space into the console to understand our output // output => blank space
+
+let text = "55";
+console.log(text) // output => 55
+console.log(typeof text) // output => string
+console.log("") // we are using this to make an space into the console to understand our output // output => blank space
+
+let stringToNumber = Number(text) // output => 55 (as number)
+console.log(stringToNumber) // output => 55
+console.log(typeof(stringToNumber)) // output => Number
+console.log("") // we are using this to make an space into the console to understand our output
+// output => blank space
+
+let nullValue = null 
+console.log(nullValue); // output => null
+console.log(typeof nullValue); // output => object
+console.log("") // we are using this to make an space into the console to understand our output
+// output => blank space
+
+let undefined; // or let undefined = undefined. both are same
+console.log(undefined); // output => undefined (means nothing is defined in this variable)
+console.log(typeof undefined); // output => undefined
+console.log("") // we are using this to make an space into the console to understand our output
+// output => blank space
+
+let blank = ""
+console.log(blank) // output => an unclear blank spaces
+console.log(typeof blank); // output => string
+console.log("") // we are using this to make an space into the console to understand our output
+// output => blank space
+
+let alphaNumaric = "45abc"
+console.log(alphaNumaric); // output => 45abc
+console.log(typeof alphaNumaric); // output => string
+console.log("") // we are using this to make an space into the console to understand our output
+// output => blank space
+
+let alphaNumaricToNumber = Number(alphaNumaric) 
+console.log(alphaNumaricToNumber); // output => NaN
+console.log(typeof alphaNumaricToNumber); // output => number
