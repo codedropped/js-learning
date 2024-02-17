4c32c7cde34ed750ffdf7cfa5effda4f3d97b01e
ReStart <azanakon2020@gmail.com>
2024-02-18 00:28:40 +0600

    objects

---
+++ 02_basics/01_objects.js
@@ -0,0 +1,15 @@
+//Singleton
+
+//Object literals
+
+
+const obj = {
+    "full name": "Rich Akon",
+    nickName: "Akon",
+    age: 5,
+    roll: 1,
+    gender: "Male",
+    isLoggedIn: true,
+    loggedDays: ["Saturday", "Friday"]
+}
+
