1e956fcbfac7b9376b6bd23f4ac0beaf0eb847b2
ReStart <azanakon2020@gmail.com>
2024-02-18 15:55:41 +0600

    functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,20 @@
+
+const display = function () { // this is anonymous function
+    console.log(display);
+}
+
+// console.log("A");
+// console.log("k");
+// console.log("o");
+// console.log("n");
+
+function abc() { } //syntax for declaration of a function()
+
+function name() {
+    console.log("A");
+    console.log("k");
+    console.log("o");
+    console.log("n");
+}
+
+name // this is a 
