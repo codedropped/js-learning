26f69bb3f6f32ee6939ca2afd78581e89ab089e7
ReStart <azanakon2020@gmail.com>
2024-02-18 23:44:31 +0600

    this and arrow function

--- 03_basics/04_this_and_arrow_function.js
+++ 03_basics/04_this_and_arrow_function.js
@@ -1,6 +1,7 @@
 
 const print = (display) => (console.log(display)) // implicit return
 
+/************************** THIS ***************************** */
 const user = {
     username: "Akon",
     price: 9999999999999,
@@ -12,6 +13,7 @@ const user = {
 
     },
 }
+print(user)
 
 print(this)//empty object
 
@@ -19,4 +21,45 @@ user.welcomeMessage()
 user.username = "Bip"
 user.welcomeMessage()
 
-print(user)
+function one() {
+    const name = "Bip"
+    print(this)
+}
+
+one()
+
+function two() {
+    const name = "Bip"
+    print(this.name)
+}
+two() // output => undefined // this keyword is not only working in function. it will work in the object only.
+
+/************************ Arrow function ************************** */
+
+const arrowFun = () => {
+    const name = "Akon"
+    print(this)
+}
+
+arrowFun()
+
+const arrowFunction = (num1, num2) => {
+    return num1 + num2
+}
+
+print(arrowFunction(5, 10))
+
+
+const arrowFunc = (num1, num2) => (num1 + num2) // implicit return
+
+print(arrowFunction(1, 2))
+
+const arrowFuncObj = (num1, num2) => ({ username: "Akon" }) // implicit return
+
+print(arrowFuncObj(1, 2))
+
+const array = [1, 2.5, 3, 5]
+
+array.forEach(function () { }) // we can use function in array like this as well
+array.forEach(() => { }) // we can use function in array like this as well
+// array.forEach(() => ()) // we can use function in array like this as well
