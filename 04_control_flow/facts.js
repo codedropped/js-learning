17b01faee7d9c65ed3db5d05f91caf2a25f40e58
ReStart <azanakon2020@gmail.com>
2024-02-19 13:54:33 +0600

    facts of truth

---
+++ 04_control_flow/facts.js
@@ -0,0 +1,6 @@
+// false == 0 // output=> true
+// 0 == "" // output=> true
+// 0 == '' // output=> true
+// false == "" // output=> true
+// false == '' // output=> true
+// true == 1 // output=> true
