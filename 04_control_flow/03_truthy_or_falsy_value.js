f90b4bbf28f734eb3e28965a0dbe030699fbd031
ReStart <azanakon2020@gmail.com>
2024-02-19 13:37:45 +0600

    truty and falsy value

---
+++ 04_control_flow/03_truthy_or_falsy_value.js
@@ -0,0 +1,22 @@
+const print = (abc) => console.log(abc)
+
+print("--")
+
+const userEmail = "akon@akon.com"
+
+if (userEmail) {
+    print("Got user mail")
+}
+else {
+    print("Don't have user email")
+}
+
+
+
+
+// truthy value
+// true, " ", "0", [], {},
+
+
+// falsy value
+//false, "", null, undefined, 0, -0, BigInt 0n,  NaN
