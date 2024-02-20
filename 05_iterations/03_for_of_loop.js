0b044e8f5e9ccbe0d8df63a4bf4a7383d9c6e25a
ReStart <azanakon2020@gmail.com>
2024-02-20 16:56:28 +0600

    for of loop

--- 05_iterations/03_for_of_loop.js
+++ 05_iterations/03_for_of_loop.js
@@ -2,6 +2,9 @@ const print = (abc) => {
     console.log(abc)
 }
 
+
+
+// for of loop for array
 const myArray = [1, 2, 3, 4, 5, 6]
 
 for (const i of myArray) { // here i is an iterator which automatically detect the length of array, auto initialize value, auto apply condition and auto update the iterations as we compare to traditional for loop.
@@ -9,6 +12,7 @@ for (const i of myArray) { // here i is an iterator which automatically detect t
 }
 
 
+// for of loop for string 
 
 const myName = "Akon, Hello World!"
 
@@ -17,4 +21,41 @@ for (const str of myName) {
         print(`The splited word for myName is: ${str}`)
         continue
     }
+}
+
+
+// for of loop in object in upper way will not work
+
+// const obj = {
+//     'name': 'Akon',
+//     'age': 5,
+//     'gender': 'Male',
+// }
+
+// print(obj)
+
+// for (const key of obj) {
+//     print(obj)
+// }
+
+
+
+// but this way we can use for of loop in an object, but the better way to use it is "for in loop" 
+// for of loop for object
+
+const obj = {
+    'name': 'Akon',
+    'age': 5,
+    'gender': 'Male',
+}
+
+print(obj)
+
+const objArry = Object.entries(obj)
+
+print(objArry)
+
+for (const [key, value] of objArry) {
+    print(`The key is: ${key} and value is: ${value}`)
+    print(`${key}, ${value}`)
 }
