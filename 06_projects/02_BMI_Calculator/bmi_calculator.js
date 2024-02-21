6174b40404d603ca02b6c95585523ac4098c6660
ReStart <azanakon2020@gmail.com>
2024-02-22 01:54:01 +0600

    BMI Calculator

--- 06_projects/02_BMI_Calculator/bmi_calculator.js
+++ 06_projects/02_BMI_Calculator/bmi_calculator.js
@@ -2,10 +2,9 @@
 
 const form = document.querySelector('form')
 
+
 form.addEventListener('submit', function (e) {
     e.preventDefault()
-
-
 
     const height = parseInt(document.querySelector('#height').value)
     const weight = parseInt(document.querySelector('#weight').value)
@@ -14,27 +13,37 @@ form.addEventListener('submit', function (e) {
 
 
     if (height == '' || height < 0 || isNaN(height)) {
+        result.style.display = "block"
+        result.style.color = "red"
         result.innerHTML = `Please enter a valid height`
     }
     else if (weight == '' || weight < 0 || isNaN(weight)) {
+        result.style.display = "block"
+        result.style.color = "red"
         result.innerHTML = `Please enter a valid weight`
     }
     else {
 
         const finalResult = (weight / ((height * height) / 10000)).toFixed(2);
 
+        result.style.display = "block"
         result.innerHTML = finalResult;
 
         if (finalResult < 18.6) {
+            report.style.display = "block"
             report.innerHTML = 'Under Weight'
+            result.style.color = "red"
             report.style.color = "red"
         }
         else if (finalResult >= 18.6 && finalResult <= 24.9) {
+            report.style.display = "block"
             report.innerHTML = 'Normal Weight'
             report.style.color = "green"
         }
         else {
+            report.style.display = "block"
             report.innerHTML = 'Over Weight'
+            result.style.color = "red"
             report.style.color = "red"
         }
     }
